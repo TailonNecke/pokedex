0cda6e92d4ace84d23245ca23cae6c99def31f63
TailonNecke <tailon.necke@gmail.com>
2021-07-09 16:46:35 -0300

    pokédex

---
+++ Front/src/pages/Dashboard/index.tsx
@@ -0,0 +1,80 @@
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+
+import { Title, Repositories, Form } from './style';
+
+interface Repository {
+  sprites:{
+  front_default: string;
+  };
+  name: string;
+  types: Tipo[];
+  };
+
+  interface Tipo {
+      type: {
+        name: string;
+    };
+  }
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepotories] = useState<Repository[]>([]);
+  const [Tipo, setType] = useState<Tipo[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`${newRepo}`);
+    const repository = response.data;
+
+    setRepotories([...repositories, repository]);
+    setType(repository.types);
+    setNewRepo('');
+  }
+
+  return(
+
+    <>
+      <Title>Pokédex</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do pokémon ou ID" />
+          <circle></circle>
+
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map(repository => (
+
+          <a key={repository.name} href="teste">
+          <img src={repository.sprites.front_default} />
+            <div>
+              <strong>Nome: {repository.name}</strong>
+
+              <p><strong>Tipo: </strong> {repository.types.map(p => (
+                <span>| {p.type.name } |</span>
+              ))}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+
+          </a>
+        ))}
+
+      </Repositories>
+
+
+
+
+
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ Front/src/pages/Dashboard/style.ts
@@ -0,0 +1,119 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 470px;
+  line-height: 56px;
+
+  margin-top: 80px;
+  `;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 3;
+    border-color: #000000;
+    border-radius: 5px 0 0 5px;
+    color: #33a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+  circle {
+    margin-left: 478px;
+    margin-top: 24px;
+    width: 24px;
+    height: 24px;
+    background: #ffffff;
+    border-radius: 50%;
+    border: 3px solid black;
+    border-color: #000000;
+    position: absolute;
+  }
+  button {
+    width: 210px;
+    height: 70px;
+    background: #C80000;
+    border-radius: 0 5px 5px 0;
+    border: 3;
+    border-color: #000000;
+    color: #fff;
+    font-weight: bold;
+
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#A00000')};
+    }
+  }
+`
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translate(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 16px;
+      border: 2px solid black;
+      border-color: #d9d9d9;
+    }
+
+    div {
+      margin: 0 16px;
+      margin-top: 7.5px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    span {
+      float: right;
+    }
+    FiChevronRight {
+      whidth: 32px;
+      height: 32px;
+      margin-right: 10px;
+    }
+
+
+  }
+  `;
